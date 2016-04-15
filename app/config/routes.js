65b85adf1e3e6aeec929b5b59c81ce8b877d38f1
cbriner <cbriner@lifetimefitness.com>
2016-04-15 17:32:04 -0600

    added prompts and prompt containers

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,12 +8,15 @@ var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 var hashHistory = ReactRouter.hashHistory;
 
 var routes = (
     <Router history={hashHistory}>
         <Route path='/' component={Main}>
             <IndexRoute component={Home} />
+            <Route path='playerOne' header='Player One' component={PromptContainer} />
+            <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
         </Route>
     </Router>
 );
