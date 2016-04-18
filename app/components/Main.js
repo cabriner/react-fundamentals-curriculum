0037fce33ebc38cfa81e8e0e273a972aacd95b22
cbriner <cbriner@lifetimefitness.com>
2016-04-18 17:25:51 -0600

    transitions added, course completed!

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,12 +2,19 @@
  * Created by cbriner on 4/15/16.
  */
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 var Main = React.createClass({
     render: function() {
         return (
             <div className='main-container'>
-                {this.props.children}
+                <ReactCSSTransitionGroup
+                    transitionName="appear"
+                    transitionTimeout={500}
+                    transitionLeaveTimeout={500}>
+                    {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
